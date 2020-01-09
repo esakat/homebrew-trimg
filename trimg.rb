e10ae4a545c8ab959d2120343003508ed1a39fba
esakat <esaka.tom@gmail.com>
2020-01-09 21:28:21 +0900

    Create trimg.rb

---
+++ trimg.rb
@@ -0,0 +1,14 @@
+TRIMG_VERSION='0.0.1'
+
+class CliSample < Formula
+  # ダウンロードファイルのURL
+  url "https://github.com/esakat/trimg/releases/download/v#{TRIMG_VERSION}/trimg_#{TRIMG_VERSION}_darwin_amd64.tar.gz"
+  homepage "https://github.com/esakat/trimg"
+  sha256 "d3a6f7a15fde364388afd5cb2235337e7e3524ab9517c647eff685e0439d054a"
+  version "v#{TRIMG_VERSION}"
+
+  def install
+    bin.install 'trimg'
+  end
+
+end
