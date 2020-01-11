3ef0bf4212edd8acc91c5d19ca7267660bee4b39
esakat <esaka.tom@gmail.com>
2020-01-11 18:32:50 +0900

    Update trimg.rb

--- trimg.rb
+++ trimg.rb
@@ -1,10 +1,10 @@
-TRIMG_VERSION='0.0.1'
+TRIMG_VERSION='0.0.2'
 
 class Trimg < Formula
   # ダウンロードファイルのURL
   url "https://github.com/esakat/trimg/releases/download/v#{TRIMG_VERSION}/trimg_#{TRIMG_VERSION}_darwin_x86_64.tar.gz"
   homepage "https://github.com/esakat/trimg"
-  sha256 "d3a6f7a15fde364388afd5cb2235337e7e3524ab9517c647eff685e0439d054a"
+  sha256 "ab23d2df95a285950cd3d8320824d5712aa4fed07cc4e054671f66f2b53a0726"
   version "v#{TRIMG_VERSION}"
 
   def install
